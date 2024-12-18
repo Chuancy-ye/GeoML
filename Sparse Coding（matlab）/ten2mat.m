8aa740e562160815bd6b1243ffe94ceb4c339430
Xiangying <51057405+Chuancy-ye@users.noreply.github.com>
2024-12-18 02:42:13 +0000

    Delete Sparse Coding（matlab） directory

--- Sparse Coding（matlab）/ten2mat.m
+++
@@ -1,7 +0,0 @@
-function mat = ten2mat( ten,dim,k)
-% Tensor unfolding for ten with size of n1*n2*...*nd
-% dim = (n1,n2,...,nd)
-% k=1,2,...,d
-mat = reshape(permute(ten,[k,1:k-1,k+1:length(dim)]),dim(k),[]);
-end
-
